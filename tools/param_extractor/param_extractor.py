deeea1a7339a37e8a5f593f45e5440cd2547c487
Dan Benz <daniel.benz@stonybrook.edu>
2021-06-11 12:00:05 -0400

    updated the script to generate .h files

--- tools/param_extractor/param_extractor.py
+++ tools/param_extractor/param_extractor.py
@@ -14,15 +14,40 @@ import csv
 
 MAX_SEQ_LEN = 320
 
-def export_param(variables: list, file_name: str):
+def export_param(variables: list, file_name: str, header : str):
     try:
         for head, var in enumerate(variables):
-            str_var = [['{0:0>8}'.format(ffth.float_to_hex(i)) for i in row] for row in var]
-            with open(file_name + f"{head}.txt", "w+", newline="") as f:
-                writer = csv.writer(f)
-                writer.writerows(str_var)
+            str_var = [["0x"+'{0:0>8}'.format(ffth.float_to_hex(i))	for i in row] for row in var]
+            f = open(file_name + ".h", "w+")
+            f.write(header+"\n")
+            for rows in str_var:
+                f.write("{")
+                for elem in rows:
+                    f.write(elem)
+                    f.write(",")
+                f.write("},\n")
+            f.write("};")
     except TypeError:
         print('not iterable')
+        
+def export_3d_param(variables: list, file_name: str, header : str):
+    try:
+        f = open(file_name + ".h", "w+")
+        f.write(header+"\n")
+        for head, var in enumerate(variables):
+            f.write("{")
+            str_var = [["0x"+'{0:0>8}'.format(ffth.float_to_hex(i))	for i in row] for row in var]
+            for rows in str_var:
+                f.write("{")
+                for elem in rows:
+                    f.write(elem)
+                    f.write(",")
+                f.write("},\n")
+            f.write("},\n")
+        f.write("};")
+    except TypeError:
+        print('not iterable')
+		
 
 def extract_qkv_weights_biases(model, layer_id, ops_id='q'):
     '''
@@ -103,26 +128,68 @@ if __name__ == '__main__':
             device=-1)
     model = qa_pipeline.model
 
-    weights, biases = extract_qkv_weights_biases(model, 2, 'v')
+    weights, biases = extract_qkv_weights_biases(model, 0, 'v')
     # transform weights and biases to numpy array for the convenience
     weights = weights.detach().cpu().numpy()
     biases = biases.detach().cpu().numpy()
-    weights_list = np.split(weights, 12, axis=-1)
-    print(weights_list[0].shape, biases.shape)
+    biases = biases.reshape(biases.shape[0],1)
+	
+    #weights_list = np.split(weights, 12, axis=-1)
+    print(weights.shape, biases.shape)
     # save numbers:
-    export_param(weights_list, "layer_2_v_weights")
+    export_param([weights], "output_files/layer_0_v_weights","uint32_t VW[W_WIDTH][SEQ_WIDTH] = {")
+    export_param([biases], "output_files/layer_0_v_bias","uint32_t VB[W_WIDTH][1] = {")
+	
+    weights, biases = extract_qkv_weights_biases(model, 0, 'k')
+    # transform weights and biases to numpy array for the convenience
+    weights = weights.detach().cpu().numpy()
+    biases = biases.detach().cpu().numpy()
+    biases = biases.reshape(biases.shape[0],1)
+	
+    #weights_list = np.split(weights, 12, axis=-1)
+    print(weights.shape, biases.shape)
+    # save numbers:
+    export_param([weights], "output_files/layer_0_k_weights","uint32_t KW[W_WIDTH][SEQ_WIDTH] = {")
+    export_param([biases], "output_files/layer_0_k_bias","uint32_t KB[W_WIDTH][1] = {")
+	
+	
+    weights, biases = extract_qkv_weights_biases(model, 0, 'q')
+    # transform weights and biases to numpy array for the convenience
+    weights = weights.detach().cpu().numpy()
+    biases = biases.detach().cpu().numpy()
+    biases = biases.reshape(biases.shape[0],1)
+	
+    #weights_list = np.split(weights, 12, axis=-1)
+    print(weights.shape, biases.shape)
+    # save numbers:
+    export_param([weights], "output_files/layer_0_q_weights","uint32_t QW[W_WIDTH][SEQ_WIDTH] = {")
+    export_param([biases], "output_files/layer_0_q_bias","uint32_t QB[W_WIDTH][1] = {")
+	
 
-    dense_weights, dense_biases = extract_attention_dense_weights_biases(model, 2)
+    dense_weights, dense_biases = extract_attention_dense_weights_biases(model, 0)
     # transform weights and biases to numpy array for the convenience
     dense_weights = dense_weights.detach().cpu().numpy()
     dense_biases = dense_biases.detach().cpu().numpy()
-    export_param([dense_weights], "layer_2_dense_weights")
+    dense_biases = dense_biases.reshape(dense_biases.shape[0],1)
+	
+    export_param([dense_weights], "output_files/layer_0_dense_weights","uint32_t ZW[SEQ_WIDTH][W_WIDTH] = {")
+    export_param([dense_biases], "output_files/layer_0_dense_bias","uint32_t ZB[W_WIDTH][1] = {")
 
     # extract input embeddings:
     embd_outputs = extract_attention_layer_inputs(qa_pipeline)
-
+    print(embd_outputs.shape)
+    export_param([embd_outputs], "output_files/layer_0_inputs","uint32_t X[320][SEQ_WIDTH] = {")
+    
     # extract q, k, v:
     q, k, v = extract_qkv(qa_pipeline)
-    print(q.shape)
+    for i_index, instance in enumerate(q):
+        for l_index, layer in enumerate(instance):
+            export_3d_param(layer, f"output_files/q_inst{i_index}_layer{l_index}", "__uint32_t SW_Q[NUM_HEADS][SEQ_LEN][W_WIDTH_PER_HEAD] = {")
+    for i_index, instance in enumerate(k):
+        for l_index, layer in enumerate(instance):
+            export_3d_param(layer, f"output_files/k_inst{i_index}_layer{l_index}", "__uint32_t SW_K[NUM_HEADS][SEQ_LEN][W_WIDTH_PER_HEAD] = {")
+    for i_index, instance in enumerate(v):
+        for l_index, layer in enumerate(instance):
+            export_3d_param(layer, f"output_files/v_inst{i_index}_layer{l_index}", "__uint32_t SW_V[NUM_HEADS][SEQ_LEN][W_WIDTH_PER_HEAD] = {")
 
-    
+    print(q.shape) #[super_seq = 1 (when queries are larger than 320)][layers = 12][heads=12][SEQ_L=15][EMB_W = 64]   
