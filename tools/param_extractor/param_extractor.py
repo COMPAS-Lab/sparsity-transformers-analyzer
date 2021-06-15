cf979415f640b7a43448c71a22b3c3a1c67f740f
Dan Benz <daniel.benz@stonybrook.edu>
2021-06-15 08:38:12 -0400

    extract all params and inputs

--- tools/param_extractor/param_extractor.py
+++ tools/param_extractor/param_extractor.py
@@ -70,7 +70,7 @@ def extract_qkv_weights_biases(model, layer_id, ops_id='q'):
 
     return weights, biases
 
-def extract_attention_dense_weights_biases(model, layer_id):
+def extract_attention_dense_weights_biases(model, layer_id, dataset):
     '''
     return: weights and biases of the attention output dense layer in pytorch tensor.
     weights shape: (768, 768)
@@ -84,12 +84,58 @@ def extract_attention_dense_weights_biases(model, layer_id):
             if split_name[1] == 'encoder' and \
                 split_name[4] == 'attention' and \
                 int(split_name[3]) == layer_id and \
-                split_name[-3] == "output" and split_name[-2] == "dense":
+                split_name[-3] == "output" and split_name[-2] == dataset:
                 weights = param if split_name[-1] == 'weight' else weights
                 biases = param if split_name[-1] == 'bias' else biases
                 print(f'{split_name[-1]} of {split_name[2]} {split_name[3]} {split_name[-3]}.{split_name[-2]} extracted.')
 
     return weights, biases
+    
+def extract_intermediate_dense_weights_biases(model, layer_id):
+    '''
+    return: weights and biases of the attention output dense layer in pytorch tensor.
+    weights shape: (768, 768)
+    biases shape: (768,)
+    '''
+    weights, biases = None, None
+    with torch.no_grad():
+        for name, param in model.named_parameters():
+            split_name = name.split('.')
+            # roberta.encoder.layer.0.attention.output.dense.weight
+            if split_name[1] == 'encoder' and \
+                split_name[4] == 'intermediate' and \
+                int(split_name[3]) == layer_id and \
+                split_name[-2] == "dense":
+                weights = param if split_name[-1] == 'weight' else weights
+                biases = param if split_name[-1] == 'bias' else biases
+                print(f'{split_name[-1]} of {split_name[2]} {split_name[3]} {split_name[-3]}.{split_name[-2]} extracted.')
+
+    return weights, biases    
+    
+    
+def extract_output_dense_weights_biases(model, layer_id, dataset):
+    '''
+    return: weights and biases of the attention output dense layer in pytorch tensor.
+    weights shape: (768, 768)
+    biases shape: (768,)
+    '''
+    weights, biases = None, None
+    with torch.no_grad():
+        for name, param in model.named_parameters():
+            split_name = name.split('.')
+            # roberta.encoder.layer.0.attention.output.dense.weight
+            if split_name[1] == 'encoder' and \
+                split_name[4] == 'output' and \
+                int(split_name[3]) == layer_id and \
+                split_name[-2] == dataset:
+                weights = param if split_name[-1] == 'weight' else weights
+                biases = param if split_name[-1] == 'bias' else biases
+                print(f'{split_name[-1]} of {split_name[2]} {split_name[3]} {split_name[-3]}.{split_name[-2]} extracted.')
+
+    return weights, biases   
+    
+    
+    
 
 def extract_attention_layer_inputs(inf_pipeline):
     '''
@@ -99,7 +145,7 @@ def extract_attention_layer_inputs(inf_pipeline):
     simple_text_test = [{'context': 'New York is in the United States', 'question': 'where is NYC?'}, 
                         {'context': 'ABCDEFGJH', 'question': 'what is the character after D?'}]
     predictions = qa_pipeline(simple_text_test[0], max_seq_len=MAX_SEQ_LEN)
-    embd_output = predictions['hidden_states'][0][0]
+    embd_output = predictions['hidden_states']
 
     res = predictions['pipeline_prbs']
     return embd_output
@@ -114,8 +160,8 @@ def extract_qkv(inf_pipeline):
     predictions = qa_pipeline(simple_text_test[0], max_seq_len=MAX_SEQ_LEN)
 
     res = predictions['pipeline_prbs']
-    q_pr, k_pr, v_pr, scrs_pr, _ = predictions['pipeline_prbs']
-    return np.stack(q_pr, axis=0), np.stack(k_pr, axis=0), np.stack(v_pr, axis=0)
+    q_pr, k_pr, v_pr, softmax_input, att_x_value = predictions['pipeline_prbs']
+    return np.stack(q_pr, axis=0), np.stack(k_pr, axis=0), np.stack(v_pr, axis=0), np.stack(softmax_input, axis=0), np.stack(att_x_value , axis=0)
 
 if __name__ == '__main__':
     # model_name = "roberta-base"
@@ -135,7 +181,6 @@ if __name__ == '__main__':
     biases = biases.reshape(biases.shape[0],1)
 	
     #weights_list = np.split(weights, 12, axis=-1)
-    print(weights.shape, biases.shape)
     # save numbers:
     export_param([weights], "output_files/layer_0_v_weights","uint32_t VW[W_WIDTH][SEQ_WIDTH] = {")
     export_param([biases], "output_files/layer_0_v_bias","uint32_t VB[W_WIDTH][1] = {")
@@ -147,7 +192,6 @@ if __name__ == '__main__':
     biases = biases.reshape(biases.shape[0],1)
 	
     #weights_list = np.split(weights, 12, axis=-1)
-    print(weights.shape, biases.shape)
     # save numbers:
     export_param([weights], "output_files/layer_0_k_weights","uint32_t KW[W_WIDTH][SEQ_WIDTH] = {")
     export_param([biases], "output_files/layer_0_k_bias","uint32_t KB[W_WIDTH][1] = {")
@@ -160,28 +204,64 @@ if __name__ == '__main__':
     biases = biases.reshape(biases.shape[0],1)
 	
     #weights_list = np.split(weights, 12, axis=-1)
-    print(weights.shape, biases.shape)
     # save numbers:
     export_param([weights], "output_files/layer_0_q_weights","uint32_t QW[W_WIDTH][SEQ_WIDTH] = {")
     export_param([biases], "output_files/layer_0_q_bias","uint32_t QB[W_WIDTH][1] = {")
 	
 
-    dense_weights, dense_biases = extract_attention_dense_weights_biases(model, 0)
+    dense_weights, dense_biases = extract_attention_dense_weights_biases(model, 0, "dense")
     # transform weights and biases to numpy array for the convenience
     dense_weights = dense_weights.detach().cpu().numpy()
     dense_biases = dense_biases.detach().cpu().numpy()
     dense_biases = dense_biases.reshape(dense_biases.shape[0],1)
 	
     export_param([dense_weights], "output_files/layer_0_dense_weights","uint32_t ZW[SEQ_WIDTH][W_WIDTH] = {")
-    export_param([dense_biases], "output_files/layer_0_dense_bias","uint32_t ZB[W_WIDTH][1] = {")
+    export_param([dense_biases], "output_files/layer_0_dense_bias","uint32_t ZB[SEQ_WIDTH][1] = {")
+    
+    dense_weights, dense_biases = extract_attention_dense_weights_biases(model, 0, "LayerNorm")
+    # transform weights and biases to numpy array for the convenience
+    dense_weights = dense_weights.detach().cpu().numpy()
+    dense_weights = dense_weights.reshape(dense_weights.shape[0],1)
+    dense_biases = dense_biases.detach().cpu().numpy()
+    dense_biases = dense_biases.reshape(dense_biases.shape[0],1)
+	
+    export_param([dense_weights], "output_files/layer_0_LayerNorm_weights","uint32_t LN1_W[SEQ_WIDTH][1] = {")
+    export_param([dense_biases], "output_files/layer_0_LayerNorm_bias","uint32_t LN1_B[SEQ_WIDTH][1] = {")
+
+    dense_weights, dense_biases = extract_intermediate_dense_weights_biases(model, 0)
+    dense_weights = dense_weights.detach().cpu().numpy()
+    dense_biases = dense_biases.detach().cpu().numpy()
+    dense_biases = dense_biases.reshape(dense_biases.shape[0],1)
+
+    export_param([dense_weights], "output_files/layer_0_intermediate_dense_weights","uint32_t Z2W[FC2_HIDDEN_SIZE][SEQ_WIDTH] = {")
+    export_param([dense_biases], "output_files/layer_0_intermediate_dense_bias","uint32_t Z2B[FC2_HIDDEN_SIZE][1] = {")
+    
+    dense_weights, dense_biases = extract_output_dense_weights_biases(model, 0, "dense")
+    dense_weights = dense_weights.detach().cpu().numpy()
+    dense_biases = dense_biases.detach().cpu().numpy()
+    dense_biases = dense_biases.reshape(dense_biases.shape[0],1)
+
+    export_param([dense_weights], "output_files/layer_0_output_dense_weights","uint32_t Z3W[SEQ_WIDTH][FC2_HIDDEN_SIZE] = {")
+    export_param([dense_biases], "output_files/layer_0_output_dense_bias","uint32_t Z3B[SEQ_WIDTH][1] = {")
+
+    dense_weights, dense_biases = extract_output_dense_weights_biases(model, 0, "LayerNorm")
+    dense_weights = dense_weights.detach().cpu().numpy()
+    dense_weights = dense_weights.reshape(dense_weights.shape[0],1)
+    dense_biases = dense_biases.detach().cpu().numpy()
+    dense_biases = dense_biases.reshape(dense_biases.shape[0],1)
+
+    export_param([dense_weights], "output_files/layer_0_output_LayerNorm_weights","uint32_t LN2_W[SEQ_WIDTH][1] = {")
+    export_param([dense_biases], "output_files/layer_0_output_LayerNorm_bias","uint32_t LN2_B[SEQ_WIDTH][1] = {")
+
 
     # extract input embeddings:
     embd_outputs = extract_attention_layer_inputs(qa_pipeline)
-    print(embd_outputs.shape)
-    export_param([embd_outputs], "output_files/layer_0_inputs","uint32_t X[320][SEQ_WIDTH] = {")
+    for i_index, instance in enumerate(embd_outputs):
+        for l_index, layer in enumerate(instance):
+            export_param([layer], f"output_files/layer_{i_index}_inputs", f"uint32_t X_{i_index}"+"[320][SEQ_WIDTH] = {")
     
-    # extract q, k, v:
-    q, k, v = extract_qkv(qa_pipeline)
+    # extract q, k, v, QK, and Z:
+    q, k, v, softmax_input, att_x_value = extract_qkv(qa_pipeline)
     for i_index, instance in enumerate(q):
         for l_index, layer in enumerate(instance):
             export_3d_param(layer, f"output_files/q_inst{i_index}_layer{l_index}", "__uint32_t SW_Q[NUM_HEADS][SEQ_LEN][W_WIDTH_PER_HEAD] = {")
@@ -191,5 +271,9 @@ if __name__ == '__main__':
     for i_index, instance in enumerate(v):
         for l_index, layer in enumerate(instance):
             export_3d_param(layer, f"output_files/v_inst{i_index}_layer{l_index}", "__uint32_t SW_V[NUM_HEADS][SEQ_LEN][W_WIDTH_PER_HEAD] = {")
-
-    print(q.shape) #[super_seq = 1 (when queries are larger than 320)][layers = 12][heads=12][SEQ_L=15][EMB_W = 64]   
+    for i_index, instance in enumerate(softmax_input):
+        for l_index, layer in enumerate(instance):
+            export_3d_param(layer, f"output_files/QK_inst{i_index}_layer{l_index}", "__uint32_t SW_QK[NUM_HEADS][SEQ_LEN][SEQ_LEN] = {")
+    for i_index, instance in enumerate(att_x_value):
+        for l_index, layer in enumerate(instance):
+            export_3d_param(layer, f"output_files/a_inst{i_index}_layer{l_index}", "__uint32_t SW_A[NUM_HEADS][SEQ_LEN][W_WIDTH_PER_HEAD] = {")  
