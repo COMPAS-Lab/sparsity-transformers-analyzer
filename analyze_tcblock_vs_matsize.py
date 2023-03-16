e5e8be28ac64f5503565ee394d2a9d9b848e69b4
Tianchu.ji <tianchu.ji@cs.stonybrook.edu>
2023-03-16 00:00:59 -0400

    more chain lengths in modeling more matrix size fragmentation and dot product util

--- analyze_tcblock_vs_matsize.py
+++ analyze_tcblock_vs_matsize.py
@@ -5,14 +5,10 @@ import numpy as np
 import hw_modeling
 import pandas as pd
 import matplotlib.pyplot as plt
-import skimage.measure
-from scipy.spatial.distance import hamming
 from typing import List, Union
 from math import ceil, floor, sqrt
-import random
-import multiprocessing
 from hw_modeling import StratixDpuModel
-from sparse_tensor_analyzer import factor_int
+import pickle
 
 def closest_factors_to_target(chain, target):
     true_chainlen = float(chain+2)
@@ -45,9 +41,15 @@ def list_closest_factors_to_target(chain, target):
     
     return factor_list
 
-def get_util(workload, hw_size, chain_len):
-    mat_a_row, mat_a_col, _, mat_b_col = workload
+def get_util(workload: dict, hw_size: tuple, chain_len: int):
+    '''
+    get_util: compute unpadded_matsize / actual_matsize after padding being computed
+    '''
+    mat_a_row, mat_a_col, _, mat_b_col = workload["size"]
+    row_spar = workload["row_sparsity"]
+    mat_a_col = ceil(mat_a_col*(1.-row_spar))
     hw_row, hw_col = hw_size
+
     hw_vec_actualsize = chain_len*20*ceil(mat_a_col/(chain_len*20.0))
     hw_matb_actual_colsize = hw_row*ceil(mat_b_col/hw_row)
     hw_mata_actual_rowsize = 3*hw_col*ceil(mat_a_row/(3.0*hw_col))
@@ -143,19 +145,47 @@ def tcchain_len_sweep():
     matplotlib.rcParams.update({'xtick.labelsize': fsize})
     matplotlib.rcParams.update({'ytick.labelsize': fsize})
     matplotlib.rcParams['lines.markersize'] = 3
-
-    sizes = [(1024, 240, 240, 1024), 
-              (1024, 2048, 2048, 1024), 
-              (1024, 2048, 2048, 8192), 
-              (1024, 8192, 8192, 2048)]
     
-    # sizes = [(2**i, 2**i, 2**i, 2**i) for i in [6, 7, 8, 10, 11, 14, 16]]
+    mats = [{"size": (1024, 2048, 2048, 1024), "row_sparsity": frac, "label": "sparse_attn_h"} \
+            for frac in np.arange(0.2, 1.0, 0.2)
+            ]
+    
+    mats += [
+            #13b
+            # {"size": (1024, 5120, 5120, 5120), "row_sparsity": 0.0, "label": "qkv proj"}, 
+            # {"size": (1024, 1024, 1024, 5120), "row_sparsity": 0.0, "label": "attxv proj"}, 
+            # {"size": (1024, 5120, 5120, 20480), "row_sparsity": 0.0, "label": "dense_ffn_fc1"}, 
+            # {"size": (1024, 20480, 20480, 5120), "row_sparsity": 0.0, "label": "dense_ffn_fc2"}, 
+            # 6.7b
+            # {"size": (1024, 4096, 4096, 4096), "row_sparsity": 0.0, "label": "qkv proj"}, 
+            # {"size": (1024, 1024, 1024, 4096), "row_sparsity": 0.0, "label": "attxv proj"}, 
+            # {"size": (1024, 4096, 4096, 16384), "row_sparsity": 0.0, "label": "dense_ffn_fc1"}, 
+            # {"size": (1024, 16384, 16384, 4096), "row_sparsity": 0.0, "label": "dense_ffn_fc2"}, 
+            # 125m
+            {"size": (1024, 768, 768, 768), "row_sparsity": 0.0, "label": "qkv proj"}, 
+            {"size": (1024, 1024, 1024, 768), "row_sparsity": 0.0, "label": "attxv proj"}, 
+            {"size": (1024, 768, 768, 3072), "row_sparsity": 0.0, "label": "dense_ffn_fc1"}, 
+            {"size": (1024, 3072, 3072, 768), "row_sparsity": 0.0, "label": "dense_ffn_fc2"}, 
+            # 350m
+            {"size": (1024, 1024, 1024, 1024), "row_sparsity": 0.0, "label": "qkv proj"}, 
+            {"size": (1024, 1024, 1024, 1024), "row_sparsity": 0.0, "label": "attxv proj"}, 
+            {"size": (1024, 1024, 1024, 4096), "row_sparsity": 0.0, "label": "dense_ffn_fc1"}, 
+            {"size": (1024, 4096, 4096, 1024), "row_sparsity": 0.0, "label": "dense_ffn_fc2"}, 
+            #1.3b
+            {"size": (1024, 2048, 2048, 2048), "row_sparsity": 0.0, "label": "qkv proj"}, 
+            {"size": (1024, 1024, 1024, 2048), "row_sparsity": 0.0, "label": "attxv proj"}, 
+            # {"size": (1024, 2048, 2048, 8192), "row_sparsity": 0.0, "label": "dense_ffn_fc1"}, 
+            # {"size": (1024, 8192, 8192, 2048), "row_sparsity": 0.0, "label": "dense_ffn_fc2"}, 
+    ]
 
-    flops_vs_chain_len = {"3": [], "6": [], "9":[], "18": []}
-    util_vs_chain_len = {"3": [], "6": [], "9":[], "18": []}
-    # tcc_array_shapes = {l: closest_factors_to_target(int(l), 2560) for l in flops_vs_chain_len.keys()}
-    tcc_array_shapes = {"3": (48, 16), "6": (24, 16), "9":(16, 16), "18": (8, 16)}
-    tcc_array_dot_percentage = {"3": 0., "6": 0., "9":0., "18": 0.}
+    mats.sort(key=lambda s: s["size"][0] * s["size"][1] * (1. - s["row_sparsity"]) * s["size"][3])
+
+    chain_len_list = {str(i): [] for i in range(2, 20, 4)}
+    flops_vs_chain_len = chain_len_list.copy()
+    util_vs_chain_len = chain_len_list.copy()
+    tcc_array_shapes = {l: closest_factors_to_target(int(l), 3960) for l in flops_vs_chain_len.keys()}
+    # tcc_array_shapes = {"3": (48, 16), "6": (24, 16), "9":(16, 16), "18": (8, 16)}
+    tcc_array_util_percentage = {k: (0., 0.) for k in chain_len_list.keys()}
 
     print(tcc_array_shapes)
 
@@ -164,18 +194,26 @@ def tcchain_len_sweep():
         tcc_array_shape = tcc_array_shapes[chain_len_str]
         dsp_percentage = tcc_array_shape[0]*tcc_array_shape[1]*(chain_len+2)/3960.0
         dot_size = tcc_array_shape[0]*tcc_array_shape[1]*chain_len/3960.0
-        tcc_array_dot_percentage[chain_len_str] = dot_size
+        tcc_array_util_percentage[chain_len_str] = (dot_size, dsp_percentage)
         print(f"actual size of len {chain_len_str}: {dsp_percentage:.3f}")
 
         #sweep across mat size
         flops, util = [], []
-        for s in sizes:
-            mat_a_row, mat_a_col, mat_b_row, mat_b_col = s
-            total_util = get_util(s, tcc_array_shape, chain_len)
+        for m in mats:
+            mat_a_row, mat_a_col, mat_b_row, mat_b_col = m["size"]
+            total_util = get_util(m, tcc_array_shape, chain_len)
+            # generate fake data to be sent to hw model:
+            fake_data = None
+            if m["row_sparsity"] > 0.0:
+                dense_acol_size = ceil(mat_a_col * (1.-m["row_sparsity"]))
+                fake_data = np.ones((mat_a_row, dense_acol_size))
+                fake_data = np.pad(fake_data, ((0, 0), (0, mat_a_col-dense_acol_size)), "constant", constant_values = (0,))
+                print(f"padded mat size: {fake_data.shape}")
+            else:
+                fake_data = np.ones((mat_a_row, mat_a_col))
 
-            fake_dense_data = np.ones((mat_a_row, mat_a_col))
             curr_model = StratixDpuModel(mat_a_row, mat_a_col, mat_b_row, mat_b_col, 
-                                        exp_dat=fake_dense_data, 
+                                        exp_dat=fake_data, 
                                         freq=300, 
                                         num_tcs=3960, 
                                         tcc_array_shape=tcc_array_shape, 
@@ -183,7 +221,7 @@ def tcchain_len_sweep():
 
             curr_model.set_tccore_size(20)
             curr_flops, curr_lat = curr_model.tensor_fpga21_mat_sparse_flops(
-                                                        fake_dense_data, \
+                                                        fake_data, \
                                                         True, False, \
                                                         using_single_column=False,
                                                         sparse_block_size=1)
@@ -193,34 +231,58 @@ def tcchain_len_sweep():
         flops_vs_chain_len[chain_len_str] = flops
         util_vs_chain_len[chain_len_str] = util
 
-    matmul_size = [i[0]*i[1]*i[3] for i in sizes]
+    matmul_size = [i["size"][0]*ceil(i["size"][1]*(1.-i["row_sparsity"]))*i["size"][3] for i in mats]
 
-    fig, axs=plt.subplots(nrows=1, ncols=2, figsize=(12, 6))
+    fig, axs=plt.subplots(nrows=1, ncols=3, figsize=(18, 6))
     for idx, clen in enumerate(flops_vs_chain_len.keys()):
         axs[0].plot(matmul_size, flops_vs_chain_len[clen], linestyle='-', 
                 marker='s', linewidth=1, alpha=0.8, label=f"chain len={clen}", color=f"C{idx}")
+        axs[1].plot(matmul_size, np.array(util_vs_chain_len[clen]) * tcc_array_util_percentage[clen][0], linestyle='-', 
+                marker='s', linewidth=1, alpha=0.8, label=f"chain len={clen}", color=f"C{idx}")
     
-    dot_percentage_bar = axs[1].bar(tcc_array_dot_percentage.keys(), 
-                [v*100.0 for v in tcc_array_dot_percentage.values()], 
+    print(flops_vs_chain_len)
+    peak_tops = 50.0 * (300./440.) * (3960./2387.) * (20./8.0)
+    axs[0].axhline(y=peak_tops, linewidth=1, color='r', linestyle='--', )
+    axs[0].text(max(matmul_size), peak_tops+5, "theoratical", fontsize=8, va='center', ha='center')
+    
+    dsp_percentage_bar = axs[2].bar(tcc_array_util_percentage.keys(), 
+                [v[1]*100.0 for v in tcc_array_util_percentage.values()], 
                 width=0.3, 
                 align="center", 
-                color=[f"C{idx}" for idx in range(len(tcc_array_dot_percentage))])
+                color="C9")
+    dot_percentage_bar = axs[2].bar(tcc_array_util_percentage.keys(), 
+                [v[0]*100.0 for v in tcc_array_util_percentage.values()], 
+                width=0.3, 
+                align="center", 
+                color=[f"C{idx}" for idx in range(len(tcc_array_util_percentage))])
         
     axs[0].set_ylabel('TOPs')
     axs[0].set_xlim(xmin=0)
+    # axs[0].set_xlim(xmax=2*1e10)
     axs[0].set_ylim(ymin=0)
-    axs[0].set_ylim(ymax=90)
+    # axs[0].set_ylim(ymax=100)
     axs[0].grid(linestyle='--', color='grey', alpha=0.5, linewidth=1)
     axs[0].set_xlabel('MAC Operations')
     axs[0].legend()
-    axs[1].bar_label(dot_percentage_bar, labels=[f"{l:.1f}" for l in dot_percentage_bar.datavalues])
-    axs[1].set_xlabel('Chain length')
-    axs[1].set_ylabel(r'TC% used for dot-product')
+
+    axs[1].set_ylabel(r'actual MAC size/padded MAC size X TC% used for dot-product')
+    axs[1].set_xlim(xmin=0)
     axs[1].set_ylim(ymin=0)
-    axs[1].set_ylim(ymax=105)
+    axs[1].set_ylim(ymax=1.1)
     axs[1].grid(linestyle='--', color='grey', alpha=0.5, linewidth=1)
+    axs[1].set_xlabel('MAC Operations')
+    axs[1].legend()
+
+    axs[2].bar_label(dot_percentage_bar, labels=[f"{l:.1f}" for l in dot_percentage_bar.datavalues])
+    axs[2].bar_label(dsp_percentage_bar, labels=[f"{l:.1f}" for l in dsp_percentage_bar.datavalues])
+    axs[2].set_xlabel('Chain length')
+    axs[2].set_ylabel(r'TC% used for dot-product')
+    axs[2].set_ylim(ymin=0)
+    # axs[1].set_ylim(ymax=105)
+    axs[2].grid(linestyle='--', color='grey', alpha=0.5, linewidth=1)
     fig.tight_layout()
-    fig.savefig("res_fig/matsize_sweep_chainlen_dynachange.pdf")
+    fig.savefig("res_fig/matsize_sweep_chainlen_fixed_totaldsp_lesshwtypes.pdf")
+    # pickle.dump(fig, open('res_fig/matsize_sweep_chainlen.fig.pickle', 'wb'))
     plt.cla()
 
 def main():
