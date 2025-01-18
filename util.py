72d4e524803359ca10d216bfcd77fcdf4e2c6d19
Tianchu Ji <tianchu.ji@stonybrook.edu>
2025-01-18 00:36:51 -0500

    enrich util function

--- util.py
+++ util.py
@@ -2,9 +2,19 @@ from os import listdir
 from os.path import isfile
 
 
-def get_pts_under_dir(base_path: str, postfix: str) -> list[str]:
-    inst_list = [f.split(".")[0] \
-                 for f in listdir(base_path) \
-                    if isfile(base_path + f) and f[0] == "i" and f.endswith("." + postfix)]
-    inst_list = [base_path + i + "." + postfix for i in inst_list]
+def get_pts_under_dir(base_path: str, postfix: str, datatype=None) -> list[str]:
+    inst_list = []
+    if base_path[-1] != "/":
+        base_path = base_path + "/"
+
+    if datatype:
+        for f in listdir(base_path):
+            if isfile(base_path + f) and f[0] == "i" and f.endswith("." + postfix):
+                if f.split("_")[-1].split(".")[0] == datatype:
+                    inst_list.append(base_path + f)
+    else:
+        for f in listdir(base_path):
+            if isfile(base_path + f) and f[0] == "i" and f.endswith("." + postfix):
+                inst_list.append(base_path + f)
+        
     return inst_list
